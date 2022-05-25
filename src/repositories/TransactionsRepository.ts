c6fee5ca30be4056c691f5a700b41b3c476ea5da
vicvictor86 <vicbem@hotmail.com>
2022-05-25 17:26:13 -0300

    Refatoração do TransactionRepository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -19,8 +19,15 @@ interface Request{
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private income: number;
+  private outcome: number;
+  private total: number;
+
   constructor() {
     this.transactions = [];
+    this.income = 0;
+    this.outcome = 0;
+    this.total = 0;
   }
 
   public all(): Transaction[] {
@@ -28,25 +35,18 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    let income = 0;
-    let outcome = 0;
-    let total = 0;
-
-    this.transactions.forEach(transaction => {
-      if(transaction.type === 'income'){
-        income += transaction.value;
-        total += transaction.value;
-      }
-      else {
-        outcome += transaction.value;
-        total -= transaction.value;
-      }
-    })
-
-    return { income, outcome, total };
+    return { income: this.income, outcome: this.outcome, total: this.total};
   }
 
   public create({ title, value, type }: Request): Transaction {
+    if(type === 'income'){
+      this.income += value;
+      this.total += value;
+    } else{
+      this.outcome += value;
+      this.total -= value;
+    }
+    
     const id = v4();
 
     const transactionRequest = { id, title, value, type };
